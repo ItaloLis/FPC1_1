641bb7fdff1ad6c1ec5e03ac975c42f33174eaee
Italo Lima <87744963+ItaloLis@users.noreply.github.com>
2023-02-02 10:31:18 -0300

    Update recursao_fibonacci.py

--- recursao_fibonacci.py
+++ recursao_fibonacci.py
@@ -1,9 +1,19 @@
+#recursao:
+#fibonacci recursivo
+#sequencia de fibonacci: 0 1 1 2 3 5 8 13 21 34 55 ...
+
 def fibRec(n):
-  if n==1: return 0
-  elif n==2: return 1
-  else: return fibRec(n-1) + fibRec(n-2)
-  
-for i in range(1, 8):
-  print(fibRec(1))
-  
-#Exemplo de recursão
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibRec(n - 1) + fibRec(n - 2)
+
+
+def menu():
+    n = int(input("Quantos termos devem ser exibidos? (maior que 2)"))
+    for i in range(1, n+1):
+        print(fibRec(i))
+
+menu()
