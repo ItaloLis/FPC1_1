d369eacf0b3d458cd229f835ccbbea9981abf700
Italo Lima <87744963+ItaloLis@users.noreply.github.com>
2023-03-09 09:39:27 -0300

    Update ordenacao_lista.py

--- ordenacao_lista.py
+++ ordenacao_lista.py
@@ -1,17 +1,3 @@
-lista = list
-lista = [28, 26, 30, 24, 25, 102, 100, 50]
-for i in range(len(lista)-1):
-    if lista[i] > lista[i+1]:
-        lista[i], lista[i+1] = lista[i+1], lista[i]
-    for r in range(len(lista)-1, 0, -1):
-        if lista[r] < lista[r-1]:
-            lista[r], lista[r-1] = lista[r-1], lista[r]
-print(lista)
-
-
-#ou
-
-
 def bubbleSort(lista, tamanho):
     troca = True
     while troca:
@@ -28,3 +14,15 @@ def main():
     print(bubbleSort(list1, len(list1)))
 
 main()
+
+#ou
+
+lista = list
+lista = [28, 26, 30, 24, 25, 102, 100, 50]
+for i in range(len(lista)-1):
+    if lista[i] > lista[i+1]:
+        lista[i], lista[i+1] = lista[i+1], lista[i]
+    for r in range(len(lista)-1, 0, -1):
+        if lista[r] < lista[r-1]:
+            lista[r], lista[r-1] = lista[r-1], lista[r]
+print(lista)
