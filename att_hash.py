2bf858fd5b653d082d9f89020fe8a3e468bbed0c
Italo Lima <87744963+ItaloLis@users.noreply.github.com>
2023-03-23 10:56:01 -0300

    Update att_hash.py

--- att_hash.py
+++ att_hash.py
@@ -1,3 +1,4 @@
+#https://www.ime.usp.br/~pf/estruturas-de-dados/aulas/st-hash.html
 #Questões - Tabelas Hash
 
 #q1
@@ -32,4 +33,25 @@ def busca(chave, tabela):
                 return elem[1]
     else:
         return None #Não achou, lista terminou
+
+
+#q6 Sondagem linear
+def InserirSondLin(chave, tabela):
+    pos = funcHash(chave, len(tabela))
+    while tabela[pos] != None:
+        pos += 1
+        if pos == len(tabela):
+            pos = 0
+    tabela[pos] = chave
+#pos = (pos + 1) % len(tabela)   substitui 3 linhas: 41 a 43
+
+
+#q7 Sondagem Quadrática
+def InserirSondQuad(chave, tabela):
+    pos = chave % len(tabela)
+    i = 0
+    while tabela[pos] != None:
+        i += 1
+        pos = (pos + i**2) % len(tabela)
+    tabela[pos] = chave
 
